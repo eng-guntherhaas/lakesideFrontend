9eab5d6b97ae5ab566120a0a036ec7fad4cde751
guntherhaas <guntherhaas.eng@gmail.com>
2024-07-26 15:22:51 +0200

    create header components

---
+++ src/components/layout/HeaderMain.jsx
@@ -0,0 +1,15 @@
+const HeaderMain = () => {
+  return (
+    <header className="header-banner">
+      <div className="overlay"></div>
+      <div className="animated-text overlay-content">
+        <h1>
+          Welcome to <span className="hotel-color">LakeSide Hotel</span>
+        </h1>
+        <h4>Experience the best hospitality in town</h4>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderMain;
